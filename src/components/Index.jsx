9df18799f05f103a525f7af7afd461015a98820c
HughOwenPanopio <panopiohughowen@gmail.com>
2023-11-20 23:28:40 +0800

    finishing home page, added box shadown to navbar and photo

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Header from '../common/Header'
+import profile from '../assets/images/profile.png'
+import { Button } from 'reactstrap'
 import '../css/index.css'
 
 const Index = () => {
@@ -7,12 +9,26 @@ const Index = () => {
     <>
     <Header/>
     <main>
-      <div className="container text-center">
-        <div className="row">
-          <div className="col"></div>
-          <div className="col"></div>
-        </div>
-      </div>
+      <section>
+          <div className="row img-detailsContainer">
+            <div className="col image-container">
+              <img src={profile} alt="" />
+            </div>
+            <div className="col details-container">
+              <h3>Hello, I'm</h3>
+              <h1>Hugh Owen Cortez Panopio</h1>
+              <h3>Full Stack Web Developer</h3>
+              <div className="buttons-container">
+                <Button id='cv'>Download CV</Button>
+                <Button id='info'>Contact Info</Button>
+              </div>
+              <div className="icon-container">
+              <i class="fa-brands fa-linkedin"></i>
+              <i class="fa-brands fa-square-github"></i>
+              </div>
+            </div>
+          </div>
+      </section>
     </main>
     </>
   )
