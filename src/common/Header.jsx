7bf53e8210e97a0a4bd66e44357b9ce37d2f980d
HughOwenPanopio <panopiohughowen@gmail.com>
2023-11-20 21:31:53 +0800

    edit photo name and import

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../css/header.css";
+import Logo from "../assets/images/Logo.png";
 import { Link } from "react-router-dom";
 import { Nav } from "reactstrap";
 
@@ -12,7 +13,7 @@ const Header = () => {
             <div className="row">
               <div className="col logo-container">
                 <Link to='/'>
-                  <img src="src/assets/images/HyuCode-Final.png" alt="" />
+                  <img src={Logo} alt="" />
                 </Link>
               </div>
               <div className="col page-container">
